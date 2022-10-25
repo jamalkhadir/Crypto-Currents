bc90a39d18b2d63e6876dd0952e7632eae50f0c4
Jamal <jamalkhadir@gmail.com>
2022-10-25 17:45:07 -0400

    Key issue tracking 1

--- pages/components/CoinList.js
+++ pages/components/CoinList.js
@@ -6,14 +6,15 @@ export default function CoinList({filteredCoins}){
             {filteredCoins.map(coin => {
                 return (
                     <Coins
-                        image = {coin?.image}
-                        name = {coin?.name}
-                        symbol = {coin?.symbol}
-                        id = {coin?.id}
-                        price = {coin?.current_price}
-                        priceChange = {coin?.price_change_percentage_24h}
-                        low = {coin?.low_24h}
-                        high = {coin?.high_24h}
+                        key = {coin.id}
+                        image = {coin.image}
+                        name = {coin.name}
+                        symbol = {coin.symbol}
+                        id = {coin.id}
+                        price = {coin.current_price}
+                        priceChange = {coin.price_change_percentage_24h}
+                        low = {coin.low_24h}
+                        high = {coin.high_24h}
                     />
                 )
             })}
